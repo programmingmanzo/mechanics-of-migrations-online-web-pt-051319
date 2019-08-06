c63d4ed54ead8acaf2ed8fbe97c0a71db338f418
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-06 19:34:05 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,12 +1,12 @@
-=begin 
-class CreateArtists < ActiveRecord::Migration[5.2]
+begin 
+class CreateArtists < ActiveRecord::Migration[4.2]
   def up
   end
  
   def down
   end
 end
-=end 
+end 
 
 # db/migrate/01_create_artists.rb
  
